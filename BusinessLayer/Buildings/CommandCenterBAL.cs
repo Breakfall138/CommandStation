47217a3a5712abc10530e0b0b3ab1c182ad0a58d
MatthiasHeck <mjheck1983@gmail.com>
2017-03-12 12:51:02 -0400

    Changed CC resource cost to null

--- BusinessLayer/Buildings/CommandCenterBAL.cs
+++ BusinessLayer/Buildings/CommandCenterBAL.cs
@@ -12,10 +12,7 @@ namespace BusinessLayer.Buildings
         public CommandCenter Build(string name)
         {
             CommandCenter cc = new CommandCenter();
-            cc.cost = new List<IResource>();
-            // TODO: Need to determine actual CC cost
-            cc.cost.Add(new Ore(0));
-            cc.cost.Add(new Gold(0));
+            cc.cost = null;
             cc.name = name;
             cc.isDestroyed = false;
             cc.level = 1;
